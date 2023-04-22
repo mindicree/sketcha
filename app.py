f0048df3d17943cdfbef55db29deb709a11ae44d
Tyler Johnson <tsumararaiko@gmail.com>
2023-04-21 21:58:30 -0400

    initialized server code

---
+++ app.py
@@ -0,0 +1,39 @@
+# imports
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+import yaml
+
+# configuration load
+try:
+    with open('config.yaml', 'r') as file:
+        config = yaml.safe_load(file)
+except Exception as e:
+    print('Could not load config file. Using Default values')
+    config = {
+        'HOST': '127.0.0.1',
+        'PORT': '5000',
+        'DEBUG': True,
+        'SECRET_KEY': 'secret!'
+    }
+
+# app initialization
+app = Flask(__name__)
+app.config['SECRET_KEY'] = config['SECRET_KEY']
+socketio =  SocketIO(app, async_mode=None)
+
+# main application route
+@app.route('/')
+def index():
+    return 'Hello World'
+
+# socket routes
+@socketio.on('connect')
+def connect():
+    print('Connected')
+
+@socketio.on('disconnect')
+def disconnect():
+    print('Disconnected')
+
+if __name__ == '__main__':
+    socketio.run(app, host=config['HOST'], port=config['PORT'], debug=config['DEBUG'])
