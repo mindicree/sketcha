8bc6fb1cec386500976863c66d9b1151cfec8d5e
Tyler Johnson <tsumararaiko@gmail.com>
2023-04-22 17:56:54 -0400

    added some google fonts to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,23 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: [
     "./templates/*.{html,js}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        gamer: ['"Press Start 2P"', ...defaultTheme.fontFamily.sans],
+        koulen: ['"Koulen"', ...defaultTheme.fontFamily.sans],
+        patrick: ['"Patrick Hand SC"', ...defaultTheme.fontFamily.sans],
+        bowlby: ['"Bowlby One SC"', ...defaultTheme.fontFamily.sans],
+        rubik: ['"Rubik Iso"', ...defaultTheme.fontFamily.sans],
+        skranji: ['"Skranji"', ...defaultTheme.fontFamily.sans],
+        expletus: ['"Expletus Sans"', ...defaultTheme.fontFamily.sans],
+      }
+    },
   },
   plugins: [],
 }
