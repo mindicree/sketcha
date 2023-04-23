c68c2bf3d2e0cfcd146c6fb5192260da69fbf98f
Tyler Johnson <tsumararaiko@gmail.com>
2023-04-23 15:04:34 -0400

    home panel done mostly

--- static/js/panel.js
+++ static/js/panel.js
@@ -1,14 +1,25 @@
-function load_panel() {
+function load_panel(reload=true) {
     toggle_loading()
     let curr_player = get_player_from_local()
+    if (!reload) {
+        update_panel_ui()
+        toggle_loading()
+        transition(screen_home_panel)
+        return
+    }
     fetch(base_url(`/get-player-data?id=${curr_player['id']}`))
     .then(res => res.json())
     .then(data => {
         player_data = data['data']
+        update_panel_ui()
         toggle_loading()
         transition(screen_home_panel)
     })
     .catch(err => {
         console.log(err)
     })
+}
+
+function update_panel_ui() {
+
 }
